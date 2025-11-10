8bcb28d644508420b4dc3c17b123feca8504140a
styazhkoV <alexsandr.styazhkov@gmail.com>
2025-11-10 18:26:23 +0500

    First Commit J SB API CRUD metods

---
+++ src/main/java/github/com/styazhkoV/CRUDapiJSB/CruDapiJsbApplication.java
@@ -0,0 +1,13 @@
+package github.com.styazhkoV.CRUDapiJSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDapiJsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruDapiJsbApplication.class, args);
+    }
+
+}
